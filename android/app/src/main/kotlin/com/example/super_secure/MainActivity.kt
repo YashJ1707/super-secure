63a2181c3162a50826b7e56dffb0bb63f6e2328b
YashJ1707 <yashjaybhaye1707@gmail.com>
2022-07-24 15:22:45 +0530

    Added Location capture service for firebase

--- android/app/src/main/kotlin/com/example/super_secure/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/super_secure/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.super_secure
 
 import androidx.annotation.NonNull
+import android.view.WindowManager.LayoutParams
+import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 class MainActivity: FlutterFragmentActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
+        window.addFlags(LayoutParams.FLAG_SECURE)
+        super.configureFlutterEngine(flutterEngine)
     }
 }
